070f6af54f4730a7b82bfbd6b922753bb6a2a6b1
zulaikhahanan <aikhx00@gmail.com>
2023-01-30 21:57:28 +0800

    make it better

--- models/TaskManagement.js
+++ models/TaskManagement.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const TaskManagementSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        
     },
     description: {
         type: String
