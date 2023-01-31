5d7d2c566f18cb49128e4e08c58a8c480f356d5a
zulaikhahanan <aikhx00@gmail.com>
2023-01-31 15:57:26 +0800

    make it better

--- models/Member.js
+++ models/Member.js
@@ -9,6 +9,11 @@ const memberSchema = new mongoose.Schema({
         type: String,
         required : true
     },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+          ref: 'TaskManagement'
+      },
+
 });
 
 
